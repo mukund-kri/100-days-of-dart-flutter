5eee6f2d55abe594efe98f17f77be800c3469583
Mukund K <mukund.kri@gmail.com>
2020-05-20 20:58:12 +0530

    Day 16

---
+++ 2020.05.20/libs-exploration/try_jinja/bin/main.dart
@@ -0,0 +1,32 @@
+/*
+ * This is mostly a copy of the example in the jinja package.
+ */
+import 'dart:io';
+
+import 'package:path/path.dart' as p;
+import 'package:jinja/jinja.dart';
+
+void main() {
+  final path = Platform.script.resolve('.').toFilePath();
+  
+  final env = Environment(
+    globals: <String, Object>{
+      'now': () {
+        final dt = DateTime.now().toLocal();
+        final hour = dt.hour < 10 ? '0${dt.hour}' : dt.hour.toString();
+        final minute = dt.minute < 10 ? '0${dt.minute}' : dt.minute.toString();
+        return '$hour:$minute';
+      },
+    },
+    loader: FileSystemLoader(path: path),
+    leftStripBlocks: true,
+    trimBlocks: true,
+  );
+
+  final template = env.getTemplate('users.html');
+
+  stdout.write(template.render(users: [
+    {'fullname': 'Jhon Doe', 'email': 'jhondoe@dev.py'},
+    {'fullname': 'Jane Doe', 'email': 'janedoe@dev.py'},
+  ]));
+}
